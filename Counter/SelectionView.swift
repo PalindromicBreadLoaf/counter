a267a4dbd0b36eec9e0a3f992e53c74afc61aa60
PalindromicBreadLoaf <plooff@tutanota.com>
2024-06-13 21:33:12 +0200

    Was big dumb and reset the max number of counters to 8. Has been fixed.

--- Counter/SelectionView.swift
+++ Counter/SelectionView.swift
@@ -22,7 +22,7 @@ struct SelectionView: View {
                     .lineLimit(nil) // Allow unlimited lines
                     .multilineTextAlignment(.center) // Center the text
 
-                Stepper(value: $numberOfCounters, in: 1...8) {
+                Stepper(value: $numberOfCounters, in: 1...10) {
                     Text("\(numberOfCounters) \(numberOfCounters == 1 ? "Counter" : "Counters")")
                         .font(.title)
                 }
