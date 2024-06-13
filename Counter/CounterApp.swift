46ac3149889e2e23c0c906a9aed5dc7fef081a0b
PalindromicBreadLoaf <plooff@tutanota.com>
2024-06-13 16:32:13 +0200

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Bread Loaf on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
